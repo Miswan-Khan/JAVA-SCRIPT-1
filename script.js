4b3acefba500a1350eb49dd446255de34b64289b
Miswan-Khan <155727857+Miswan-Khan@users.noreply.github.com>
2024-01-24 23:44:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Assignment:1
+var Name = "M Miswan";
+var age = "I am 16 years old";
+var isStudent = "Yes i am a student";
+console.log(Name);
+console.log(age);
+console.log(isStudent);
+
+alert("WELCOME TO MY WEBPAGE, MY NAME IS M MISWAN, I AM A STUDENT, I AM LEARNING THE WEB DEVELOPMENT COURSE")
+// Assignment:1
